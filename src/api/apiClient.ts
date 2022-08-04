579bdea2e32a0c2daf60ac4997dbe25e6ba70183
kimhyunjo <alsxmqkqh23@gmail.com>
2022-08-04 18:43:24 +0900

    💩 Prepare deploy

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
-import { API_BASE_URL } from '../../global';
 
 const apiClient = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: import.meta.env.REACT_APP_API_BASE_URL,
   withCredentials: true,
 });
 
