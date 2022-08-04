4eca795bf97d297bc9b2528576101cfe57235eaf
“Kim-Hyunjo” <alsxmqkqh23@gmail.com>
2022-08-05 00:07:11 +0900

    ✨ Set env variables

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.REACT_APP_API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
 });
 
