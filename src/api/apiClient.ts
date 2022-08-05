9d879979f516dd37e21cd30157c35e26ddc17059
kimhyunjo <alsxmqkqh23@gmail.com>
2022-08-05 13:26:53 +0900

    🚑 Move api base url from .env to apiClient file

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: 'https://dev.api.csbroker.io/api',
   withCredentials: true,
 });
 
