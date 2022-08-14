2dca2ebc2b59d0414f5db509eff5438669e25b76
“Kim-Hyunjo” <alsxmqkqh23@gmail.com>
2022-08-14 22:37:13 +0900

    🐛 Fix comparison error

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,7 +2,7 @@ export const roundToSecondDigit = (num: number) => Math.round(num * 100) / 100;
 
 export const parseDateTime = (str: string) => {
   const dateString = Date.parse(str);
-  if (dateString !== NaN) {
+  if (!isNaN(dateString)) {
     const newDate = new Date(dateString);
     return `${newDate.getFullYear()}년 ${newDate.getMonth()}월 ${newDate.getDate()}일 ${newDate.getHours()}시 ${newDate.getMinutes()}분`;
   } else {
