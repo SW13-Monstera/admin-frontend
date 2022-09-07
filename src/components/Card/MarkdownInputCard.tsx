2fdb9d7e328b3dd752204c06e17c47df73dd0439
“Kim-Hyunjo” <alsxmqkqh23@gmail.com>
2022-09-08 01:12:07 +0900

    ♻️ Refactor MarkdownInputCard

--- src/components/Card/MarkdownInputCard.tsx
+++ src/components/Card/MarkdownInputCard.tsx
@@ -1,5 +1,6 @@
 import { Card, TextField, Typography } from '@mui/material';
 import { ChangeEvent } from 'react';
+import { useMarkdownInput } from '../../hooks/useMarkdownInput';
 import { MarkdownCard } from './MarkdownCard';
 
 interface IMarkdownCard {
@@ -10,19 +11,14 @@ interface IMarkdownCard {
   onChange: (event: ChangeEvent<HTMLInputElement>) => void;
 }
 
-export const MarkdownInputCard = ({
-  id,
-  title,
-  children,
-  defaultValue,
-  onChange,
-}: IMarkdownCard) => {
+export const MarkdownInputCard = ({ id, title, defaultValue }: IMarkdownCard) => {
+  const { value, handleValueChange } = useMarkdownInput(defaultValue);
   return (
     <Card sx={{ p: 2, my: 1 }} variant='outlined'>
       <Typography color='text.secondary' sx={{ fontSize: 12 }}>
         {title}
       </Typography>
-      <MarkdownCard>{children}</MarkdownCard>
+      <MarkdownCard>{value}</MarkdownCard>
       <TextField
         id={id}
         label='입력란'
@@ -31,7 +27,7 @@ export const MarkdownInputCard = ({
         sx={{ my: 2, width: '100%' }}
         InputLabelProps={{ shrink: true }}
         inputProps={{ maxLength: 300 }}
-        onChange={onChange}
+        onChange={handleValueChange}
         variant='filled'
       />
     </Card>
