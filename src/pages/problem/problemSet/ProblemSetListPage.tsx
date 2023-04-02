efd565cf0f1b4efda45ee9d8e73395fe8c358b5b
kimhyunjo <alsxmqkqh23@gmail.com>
2023-04-02 23:49:33 +0900

    :lipstick: Update problem set list page appbar

--- src/pages/problem/problemSet/ProblemSetListPage.tsx
+++ src/pages/problem/problemSet/ProblemSetListPage.tsx
@@ -1,22 +1,14 @@
 import { URL, URLWithParam } from '../../../constants/url';
-import { shortProblemApiWrapper } from '../../../api/wrapper/problem/shortProblemApiWrapper';
-import { useFilter } from '../../../hooks/useFilter';
 import { useTable } from '../../../hooks/useTable';
-import { ProblemListPageTemplate } from '../ProblemListPageTemplate';
-import { problemSetTableHeads, shortTableHeads } from '../../../constants/tableHeads';
+import { problemSetTableHeads } from '../../../constants/tableHeads';
 import { useQuery } from 'react-query';
-import { IShortProblemListResponse } from '../../../types/problem/shortApi';
-import { useTableParams } from '../../../hooks/useTableParams';
-import { IProblemListRequest } from '../../../types/problem/api';
 import PageTemplate from '../../../templates/PageTemplate';
 import { Box, Button, Typography } from '@mui/material';
-import { Appbar } from '../../../components/FormGroup/Appbar';
 import { Link } from 'react-router-dom';
 import { BaseTable } from '../../../components/Table/BaseTable';
 import { problemSetApiWrapper } from '../../../api/wrapper/problem/problemSetApiWrapper';
 
 export const ProblemSetListPage = () => {
-  const filterHandler = useFilter();
   const { page, handleChangePage, handleRowClick } = useTable(URLWithParam.SHORT_PROBLEM_DETAIL);
   const { data } = useQuery(['problemSetList'], problemSetApiWrapper.getProblemSet);
 
@@ -33,9 +25,11 @@ export const ProblemSetListPage = () => {
       >
         <Typography>문제 세트</Typography>
         <Link to={URL.PROBLEM_SET_ADD}>
-          <Button variant='contained' sx={{ height: '100%' }}>
-            문제 세트 추가
-          </Button>
+          <Box sx={{ textAlign: 'right' }}>
+            <Button variant='contained' sx={{ width: 150 }}>
+              문제 세트 추가
+            </Button>
+          </Box>
         </Link>
       </Box>
       <BaseTable
